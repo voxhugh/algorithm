43ceb7057b1736be193908ac506cc53cd49a0e44
voxhugh <13802530+voxhugh@user.noreply.gitee.com>
2024-05-30 17:14:53 +0800

    优化二叉堆

--- 二叉堆/二叉堆.cpp
+++ 二叉堆/二叉堆.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-#define MINSIZE 10
 #define ERROR -1
 
 typedef struct heapnode
@@ -9,69 +8,61 @@ typedef struct heapnode
 	int size;//大小
 	int* values;//数组
 }HeapNode, * BinHeap;
-//创建
-void crtHeap(BinHeap& heap, int nums[], int length, int MaxSize)
+//交换
+void swap(int* a, int* b)
 {
-	if (MaxSize < MINSIZE)
-	{
-		cout << "容量过小！" << endl;
-		return;
-	}
-	heap = new HeapNode;
-	heap->capacity = MaxSize;
-	heap->size = length;
-	heap->values = new int[MaxSize + 1];
-	heap->values[0] = -1;
-	for (int i = 1; i <= heap->size; i++)
-	{
-		heap->values[i] = nums[i - 1];
-	}
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
-//重建堆（注意：以大根堆为例）
-void sift(BinHeap& heap, int root, int length)
+//向下调整堆
+void adjust_down(BinHeap& heap, int root, int length)
 {
-	//备份根
-	int temp = heap->values[root];
-	heap->values[root] = heap->values[length];
-	int i = root;//当前结点
-	int j = 2 * i;//左孩子
-	while (j < length)
+	int F = root;//当前结点
+	int N = 2 * F;//左子结点
+	while (N < length)
 	{
-		if (j + 1 < length)
+		if (N + 1 < length && heap->values[N] < heap->values[N + 1])
 		{
-			heap->values[j] < heap->values[j + 1] ? j++ : j;
+			N++;
 		}
-		if (heap->values[i] < heap->values[j])
+		if (heap->values[F] < heap->values[N])
 		{
-			heap->values[i] = heap->values[j];
+			swap(&heap->values[F], &heap->values[N]);
+			F = N;
+			N = 2 * F;
 		}
 		else
 		{
 			break;
 		}
-		i = j;
-		j = 2 * i;
 	}
-	heap->values[i] = temp;
 }
-//初始化
-void initHeap(BinHeap& heap)
+//创建并初始化
+void crtHeap(BinHeap& heap, int MaxSize)
 {
-	for (int i = heap->size / 2; i > 0; i--)
+	int nums[] = { 48,62,35,77,55,14,35,98 };
+	heap = new HeapNode;
+	heap->capacity = MaxSize;
+	heap->size = sizeof(nums) / sizeof(nums[0]);
+	heap->values = new int[MaxSize];
+	for (int i = 0; i < heap->size; i++)
 	{
-		sift(heap, i, heap->size);
+		heap->values[i] = nums[i];
+	}
+	for (int i = heap->size / 2 - 1; i >= 0; i--)
+	{
+		adjust_down(heap, i, heap->size);
 	}
 }
 //堆排序
 void heapSort(BinHeap& heap)
 {
-	initHeap(heap);//建始堆
-	for (int i = heap->size; i > 1; i--)
+	int end = heap->size;
+	while (end > 1)
 	{
-		int temp = heap->values[1];
-		heap->values[1] = heap->values[i];
-		heap->values[i] = temp;
-		sift(heap, 1, i);
+		swap(&heap->values[0], &heap->values[end - 1]);//先首尾交换
+		adjust_down(heap, 0, --end);//然后去尾 自根向下调整
 	}
 }
 //插入
@@ -96,16 +87,15 @@ int delMax(BinHeap& heap)
 	{
 		return ERROR;
 	}
-	int temp = heap->values[1];
-	heap->values[1] = heap->values[heap->size];
-	sift(heap, 1, --heap->size);
+	int temp = heap->values[0];
+	heap->values[0] = heap->values[heap->size - 1];
+	adjust_down(heap, 0, --heap->size);
 	return temp;
 }
 int main()
 {
 	BinHeap heap;
-	int nums[] = { 48,62,35,77,55,14,35,98 };
-	crtHeap(heap, nums, sizeof(nums) / sizeof(nums[0]), 30);
+	crtHeap(heap, 30);
 	heapSort(heap);
 	system("pause");
 	return 0;
